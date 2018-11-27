296626083a07d2f10281e6d8924f0f16bfa58d47
= <=>
2018-11-27 12:43:36 +0700

    editted meet schema for database

--- src/model/MeetSchema.ts
+++ src/model/MeetSchema.ts
@@ -1,21 +1,24 @@
 import * as mongoose from 'mongoose'
 
 interface IMeet extends mongoose.Document {
-    _id : mongoose.Schema.Types.ObjectId
+    _id : string
     jam_awal : string
     jam_akhir : string
     keterangan : string
     status : string
-    dosen : mongoose.Schema.Types.ObjectId
+    mahasiswa : string
+    dosen : string
 }
 
 const Meetschema = new mongoose.Schema({
-    _id : { type : mongoose.Schema.Types.ObjectId },
+    _id : { type : String },
     jam_awal : { type : String },
     jam_akhir : { type : String },
     keterangan : { type : String },
-    status : { type : String, default : 'Progress' },
-    dosen : { type : mongoose.Schema.Types.ObjectId }
+    status : { type : String, default : 'Diajukan' },
+    catatan : { type : String, default : '' },
+    mahasiswa : { type : String },
+    dosen : { type : String }
 })
 
 export default mongoose.model<IMeet>('Meet', Meetschema)
